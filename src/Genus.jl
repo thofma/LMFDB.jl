cbd909aa5cd469d992ec2a381638222a82d4d89c
Tommy Hofmann <thofma@gmail.com>
2026-02-25 18:03:38 -0500

    adsD

--- src/Genus.jl
+++ src/Genus.jl
@@ -1,5 +1,20 @@
 #(:det, :disc, :id, :level, :rank, :nplus, :is_even, :discriminant_form, :discriminant_group_invs, :conway_symbol, :label, :rep, :mass, :class_number, :adjacency_matrix, :adjacency_polynomials, :dual_conway_symbol)
 
+function _genera_from_records(db, records::Vector{<:NamedTuple})
+  res = ZZGenus[]
+  #reps = integer_lattices(db; genus_label = ==(label))
+  genus_labels = [r[:label] for r in records]
+  reps = integer_lattices(db; genus_label = in(genus_labels))
+  for record in records
+    label = record[:label]
+    repsG = filter(r -> get_attribute(r, :lmfdb_genus_label) == label, reps)
+    G = Hecke.genus(repsG[1])
+    set_attribute!(G, :representatives => repsG)
+    push!(res, G)
+  end
+  return res
+end
+
 function _genus_from_record(db, record::NamedTuple)
   label = record[:label]
   reps = integer_lattices(db; genus_label = ==(label))
@@ -19,5 +34,5 @@ end
 
 function genera(db; kw...)
   res = LMFDBLite.search(db, "lat_genera"; kw...)
-  return _genus_from_record.(Ref(db), res)
+  return _genera_from_records(db, res)
 end
--- src/Lattice.jl
+++ src/Lattice.jl
@@ -2,6 +2,10 @@ function _lattice_from_record(record::NamedTuple)
   r = Int(record[:rank])
   G = matrix(ZZ, r, r, record[:gram])
   L = integer_lattice(;gram = G)
+  if record[:genus_label] != nothing
+    set_attribute!(L, :lmfdb_genus_label => record[:genus_label])
+  end
+  set_attribute!(L, :lmfdb_label => record[:label])
   return L
 end
 
